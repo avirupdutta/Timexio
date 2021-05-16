02fe40ba32c4ae67bb80b859119c17635868f0d1
DeboDevelop <debajyotidofficial@gmail.com>
2021-05-16 17:05:08 +0530

    Added Wishlist feature in database.

--- models/Product.js
+++ models/Product.js
@@ -1,50 +1,54 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	specs: {
-		type: String,
-		required: true
-	},
-	price: {
-		type: Number,
-		required: true
-	},
-	images: {
-		type: Array,
-		required: true
-	},
-	quantity: {
-		type: Number,
-		required: true
-	},
-	tax: {
-		type: Number,
-		required: true
-	},
-	category: {
-		type: String,
-		required: true
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	sold: {
-		type: Number,
-		default: 0
-	},
-	increasedMRP: {
-		type: Number,
-		required: true,
-	},
-	featured: {
-		type: Boolean,
-		default: false
-	}
+    name: {
+        type: String,
+        required: true,
+    },
+    specs: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    images: {
+        type: Array,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+    tax: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+    sold: {
+        type: Number,
+        default: 0,
+    },
+    increasedMRP: {
+        type: Number,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+    maillist: {
+        type: Array,
+        default: [],
+    },
 });
 
 const Product = mongoose.model("Product", productSchema);
